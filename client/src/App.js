16ae94951d8ea4909db06757d123d70086c85f71
Rob Huzzey <robert.huzzey@trussle.com>
2021-11-27 20:58:46 +0000

    init

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState('');
+  useEffect(() => {
+    const getData = async () => {
+      const f = await fetch('/api/data');
+      const data = await f.json();
+      setData(data);
+    }
+    getData();
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Response from api:
+          <div><code>{JSON.stringify({ data }, true, 2)}</code></div>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
